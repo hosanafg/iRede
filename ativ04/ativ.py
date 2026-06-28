cf6ddf7ef440f44feacc72b16be78b034740f5ce
hosanafg <fernandeshosana48@gmail.com>
2026-06-28 17:46:44 -0300

    finalizando atividade: melhoria do pipeline

--- ativ04/ativ.py
+++ ativ04/ativ.py
@@ -101,6 +101,59 @@ def suavizacao_gaussiana_pequena(img_ruidosa):
 def filtrar_mediana(img_cinza, ksize=7):
     return cv2.medianBlur(img_cinza, ksize)
 
+def morfologia_abertura(img_binaria, tamanho_kernel=3):
+    """
+    Aplica a operação de Abertura (Erosão seguida de Dilação).
+    Excelente para eliminar ruídos brancos isolados no fundo (limpar o fundo).
+    """
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (tamanho_kernel, tamanho_kernel))
+    img_processada = cv2.morphologyEx(img_binaria, cv2.MORPH_OPEN, kernel)
+    return img_processada
+
+def morfologia_fechamento(img_binaria, tamanho_kernel=3):
+    """
+    Aplica a operação de Fechamento (Dilação seguida de Erosão).
+    Excelente para preencher buracos pretos internos e conectar fendas no objeto.
+    """
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (tamanho_kernel, tamanho_kernel))
+    img_processada = cv2.morphologyEx(img_binaria, cv2.MORPH_CLOSE, kernel)
+    return img_processada
+
+def extrair_area(prop_regiao):
+    """Função 1: Extrai a área (número de pixels) do objeto."""
+    return prop_regiao.area
+
+def extrair_perimetro(prop_regiao):
+    """Função 2: Extrai o perímetro do contorno do objeto."""
+    return prop_regiao.perimeter
+
+def extrair_centroide(prop_regiao):
+    """Função 3: Extrai o centro de massa (Y, X) do objeto."""
+    y, x = prop_regiao.centroid
+    return f"({x:.1f}, {y:.1f})"
+
+def extrair_momentos_hu(prop_regiao):
+    """
+    Função 4: Extrai os Momentos de Hu (invariantes a escala, rotação e translação).
+    Retorna o primeiro momento (hu[0]) como métrica de forma principal.
+    """
+    hu_momentos = prop_regiao.moments_hu
+    return hu_momentos[0]
+
+def export_to_csv(nome):
+    try:
+        df_caracteristicas.to_csv(nome_arquivo, index=False)
+        
+        print(f"\n[DEBUG] [SUCESSO] Arquivo gerado com sucesso!")
+        print(f"[INFO] Localização: {os.path.abspath(nome_arquivo)}")
+
+    except PermissionError:
+        print(f"\n[DEBUG] [ERRO] Não foi possível salvar '{nome_arquivo}'.")
+
+    except Exception as e:
+        print(f"\n[DEBUG] [FALHA] Falha inesperada ao tentar criar o arquivo CSV.")
+        print(f"[DEBUG] Detalhes do erro: {e}")
+
 
 """ 
 ====================
@@ -131,7 +184,8 @@ limiar=threshold_otsu(imagem_pre_process["imagem_filtrada"])
 
 # segmentação binária
 #segmentada = imagem > limiar
-segmentada = (imagem_pre_process["imagem_filtrada"] > limiar).astype(np.uint8) * 255
+#segmentada = (imagem_pre_process["imagem_filtrada"] > limiar).astype(np.uint8) * 255 --> sem inv. binária
+segmentada = (imagem_pre_process["imagem_filtrada"] <= limiar).astype(np.uint8) * 255 #com inversão binária
 
 #plots
 dados_imagem=calcular_metricas_imagem(imagem)
@@ -158,3 +212,99 @@ PARTE 3
 ====================
 """
 #operações MORFOLÓGICAS
+imagem_abertura = morfologia_abertura(segmentada, tamanho_kernel=3)
+imagem_fechamento = morfologia_fechamento(imagem_abertura, tamanho_kernel=3)
+
+#dados_original = calcular_metricas_imagem(imagem)
+dados_abertura=calcular_metricas_imagem(imagem_abertura)
+dados_morfologia = calcular_metricas_imagem(imagem_fechamento)
+
+exibir_plots(
+    dados_abertura, 'Após Abertura', 
+    dados_morfologia, 'Após Abertura e Fechamento'
+)
+
+""" 
+=============
+COMENTÁRIOS
+=============
+A operação de abertura não foi muito eficaz em remover pequenos ruídos presentes na imagem binária.
+O fechamento foi utilizado para preencher pequenas falhas existentes no rosto e no fundo da imagem
+A imagem ficou um pouco mais uniforme e adequada para a etapa de identificação. 
+"""
+
+""" 
+====================
+PARTES 4 E 5
+====================
+"""
+# 4 identificação de OBJETOS
+# 5 extração de CARACTERÍSTICAS
+
+labels = label(imagem_fechamento)
+propriedades = regionprops(labels)
+
+# Lista para armazenar os dicionários de dados de cada linha da tabela
+dados_tabela = []
+contador_objetos_reais = 1
+
+fig, ax = plt.subplots(figsize=(10, 8))
+ax.imshow(imagem, cmap='gray')
+
+for prop in propriedades:
+    if prop.area < 500: 
+        continue
+
+    area_obj = extrair_area(prop)
+    perimetro_obj = extrair_perimetro(prop)
+    centroide_obj = extrair_centroide(prop)
+    hu1_obj = extrair_momentos_hu(prop)
+    
+    # Armazena p/ tabela
+    dados_tabela.append({
+        "Objeto ID": contador_objetos_reais,
+        "Área (pixels)": area_obj,
+        "Perímetro (pixels)": round(perimetro_obj, 2),
+        "Centroide (X, Y)": centroide_obj,
+        "Momento de Hu (h1)": f"{hu1_obj:.6e}" 
+    })
+    
+    y0, x0 = prop.centroid
+    ax.plot(x0, y0, 'ro', markersize=6)
+    ax.text(x0 + 5, y0 - 5, str(contador_objetos_reais), color='yellow', fontsize=12, weight='bold')
+    
+    contador_objetos_reais += 1
+
+ax.set_title("Objetos Identificados para Extração de Características")
+ax.axis('off')
+plt.show()
+
+df_caracteristicas = pd.DataFrame(dados_tabela)
+nome_arquivo = "extracao_caracteristicas_objetos.csv"
+export_to_csv(nome_arquivo)
+
+""" 
+=============
+COMENTÁRIOS
+=============
+Os objetos foram parcialmente identificados com sucesso, provavelmente devido aos pequenos buracos que ainda
+persistiram após o fechamento morfológico.
+
+"""
+
+""" 
+====================
+PARTE 6
+====================
+
+#conclusão
+É a característica mais informativa para separação de escala (macro vs. micro). 
+Ela escancara que o Objeto 1 não pertence à mesma categoria dos outros dois.
+
+Inicialmente, a imagem foi segmentada com limearização global utilizando o threshold de OTSU. Ao executar
+todos os passos, os dados do CSV revelam uma falha crucial no pipeline adotado: O Cameraman não havia sido detectado como um objeto.
+Para resolver esse problema, aplicamos uma inversão binária logo após a limearização.
+
+Agora, os dados do csv estão consistentes: o objeto 1 é o homem e o objeto 2 o tripé.
+
+"""
